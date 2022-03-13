17e95704f69bf8551c038ab2d719ac5b2a2c6861
송진영 <77527453+jinyeong-afk@users.noreply.github.com>
2022-03-13 23:16:43 +0900

    🔧 config: 프로젝트 초기 설정

---
+++ core/src/main/java/user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `spring`.`user` (
+                      `user_id` INT NOT NULL AUTO_INCREMENT,
+                      `user_name` VARCHAR(45) NOT NULL,
+                      `user_pass` VARCHAR(45) NOT NULL,
+                      `user_tel` VARCHAR(45) NULL,
+                      PRIMARY KEY (`user_id`));
+user
+`spring`.`user`
---
+++ core/src/test/java/com/phonedygaurd/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phonedygaurd.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
