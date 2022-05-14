683dcbb04c73b2d01f8d8da2a05c9b0a0c451617
jinyeong-afk <xoeh7002@naver.com>
2022-05-14 16:20:02 +0900

    ☔️ test: testController 추가

---
+++ core/src/main/java/com/phonedygaurd/core/v1/dto/request/TestDto.java
@@ -0,0 +1,11 @@
+package com.phonedygaurd.core.v1.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestDto {
+    private int testInt;
+    private String testStr;
+}
