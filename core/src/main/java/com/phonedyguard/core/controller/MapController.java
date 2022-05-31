9e958ddc45b6e1de9de51ada4bcabc8aed2d235b
Duhyeon Kim <engus4022@naver.com>
2022-06-01 01:46:12 +0900

    ✨ feat: 안심경로 저장, 현재위치 기능 수정

--- core/src/main/java/com/phonedyguard/core/controller/MapController.java
+++ core/src/main/java/com/phonedyguard/core/controller/MapController.java
@@ -1,14 +1,19 @@
 package com.phonedyguard.core.controller;
 
 import com.phonedyguard.core.v1.dto.map.MapDto;
+import com.phonedyguard.core.v1.dto.map.MapSafeDto;
 import com.phonedyguard.core.v1.repository.MapRepository;
 import com.phonedyguard.core.v1.service.MapService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,12 +24,13 @@ public class MapController {
         this.mapService = mapService;
         this.mapRepository = mapRepository;
     }
-    
+
     @Autowired
     private final MapRepository mapRepository;
 
-    @PostMapping(value = "/maps")
-    public ResponseEntity<?> getMappingTest(@RequestBody MapDto mapDto) {
+
+    @PostMapping(value = "/maps/indices")
+    public ResponseEntity<?> indices(@RequestBody MapDto mapDto) {
         mapDto = MapDto.builder()
                 .latitude(mapDto.getLatitude())
                 .longitude(mapDto.getLongitude())
@@ -33,4 +39,22 @@ public class MapController {
         mapService.savePosition(mapDto);
         return ResponseEntity.ok(mapDto);
     }
+
+    @PostMapping(value = "/maps/routes")
+    public ResponseEntity<?> saveroutes(@RequestBody MapSafeDto mapsafeDto) {
+        mapsafeDto = MapSafeDto.builder()
+                .latitude(mapsafeDto.getLatitude())
+                .longitude(mapsafeDto.getLongitude())
+                .build();
+        log.info("test: " + mapsafeDto);
+        mapService.saveSaferoutes(mapsafeDto);
+        return ResponseEntity.ok(mapsafeDto);
+    }
+
+    @GetMapping("/maps/routes")
+    public ResponseEntity<List> routeslist(){
+        List<MapSafeDto> mapSafeDtoList = mapService.getMaplist();
+        return new ResponseEntity<List>(mapSafeDtoList, HttpStatus.OK);
+    }
+
 }
